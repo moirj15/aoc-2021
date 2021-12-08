fa87a09108bd1b3d04571ea47db772d458bd8de2
jimmy moir <moirj15@gmail.com>
2021-12-08 17:40:52 -0500

    work on day3

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,41 @@
 use std::fs::File;
-use std::io::{self, BufRead, Error};
+use std::io::{prelude::*, self, Error};
 
 use std::path::Path;
 
-
 fn main() -> Result<(), Error> {
     let input_path = Path::new("input.txt");
-    let file = File::open(&input_path).unwrap();
-    let file_reader = io::BufReader::new(file);
+    let mut file = File::open(&input_path).unwrap();
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents).unwrap();
+    let bit_strings: Vec<&str> = file_contents.split_whitespace().collect();
 
-    let mut horizontal_position: u32 = 0;
-    let mut depth: u32 = 0;
-    let mut aim: u32 = 0;
+    let mut one_digit_counts: [u32; 12] = [0; 12];
+    let mut zero_digit_counts: [u32; 12] = [0; 12];
 
-    for line in file_reader.lines() {
-        if let Ok(direction_scalar) = line {
-            let split: Vec<&str> = direction_scalar.split(' ').collect();
-            match split[0] {
-                "forward" => {
-                    let x = split[1].parse::<u32>().unwrap();
-                    horizontal_position += x;
-                    depth += aim * x;
-                },
-                "down" => {
-                    aim += split[1].parse::<u32>().unwrap();
-                },
-                "up" => {
-                    aim -= split[1].parse::<u32>().unwrap();
-                },
-                _ => panic!("Unknown direction")
+    for bit_string in bit_strings {
+        for (i, bit) in bit_string.char_indices() {
+            match bit {
+                '0' => zero_digit_counts[i] += 1,
+                '1' =>  one_digit_counts[i] += 1,
+                _ => panic!("Unknown bit")
             }
         }
     }
-    println!("{}", horizontal_position * depth);
+
+    // Made of the most common bits
+    let mut gamma_rate: u32 = 0;
+    // Made of the least common bits
+    let mut epsilon: u32 = 0;
+    for i in 0..12 {
+        if zero_digit_counts[i] > one_digit_counts[i] {
+            gamma_rate <<= 1;
+            epsilon = (epsilon << 1) | 1;
+        } else {
+            epsilon <<= 1;
+            gamma_rate = (gamma_rate << 1) | 1;
+        }
+    }
+    println!("{}", gamma_rate * epsilon);
     Ok(())
 }
